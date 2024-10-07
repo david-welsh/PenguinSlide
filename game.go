81e8e96a7ce9260d4ce196910e0ee7d26f64487f
David Welsh <mail@davidwelsh.co.uk>
2024-10-07 22:10:59 +0100

    Add debug text

--- game.go
+++ game.go
@@ -1,7 +1,11 @@
 package main
 
 import (
+	"errors"
+	"fmt"
 	"github.com/hajimehoshi/ebiten/v2"
+	"github.com/hajimehoshi/ebiten/v2/ebitenutil"
+	"github.com/hajimehoshi/ebiten/v2/inpututil"
 	_ "image/png"
 )
 
@@ -14,6 +18,7 @@ type Game struct {
 	Width, Height int
 	Screen        *ebiten.Image
 	World         *World
+	Debug         bool
 }
 
 func (g *Game) Layout(_, _ int) (screenWidth, screenHeight int) {
@@ -21,12 +26,36 @@ func (g *Game) Layout(_, _ int) (screenWidth, screenHeight int) {
 }
 
 func (g *Game) Update() error {
+	var quit error
+
 	g.World.Update()
-	return nil
+
+	if inpututil.IsKeyJustPressed(ebiten.KeyF1) {
+		g.Debug = !g.Debug
+	}
+
+	if inpututil.IsKeyJustPressed(ebiten.KeyEscape) {
+		quit = errors.New("quit")
+	}
+
+	return quit
+}
+
+func generateDebugString() string {
+	fps := fmt.Sprintf("%d FPS", int(ebiten.ActualFPS()))
+	screenSize := fmt.Sprintf("Screen: %dx%d", ScreenWidth, ScreenHeight)
+	return fmt.Sprintf("%s\n%s\n", fps, screenSize)
+}
+
+func drawDebugText(screen *ebiten.Image) {
+	ebitenutil.DebugPrint(screen, generateDebugString())
 }
 
 func (g *Game) Draw(screen *ebiten.Image) {
 	g.World.Draw(screen)
+	if g.Debug {
+		drawDebugText(screen)
+	}
 }
 
 func NewGame() (*Game, error) {
